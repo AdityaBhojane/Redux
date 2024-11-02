452372f8e32632ce1e00d678f4dff56253bac6ed
Aditya Bhojane <118344077+AdityaBhojane@users.noreply.github.com>
2024-11-02 10:27:32 +0000

    first commit

---
+++ Reducer.js
@@ -0,0 +1,27 @@
+/***
+ * Reducer is design pattern where we can have a function
+    which take initial object and action as a parameter and action 
+    has two job to do 1st name of the action which we have to perform
+    and 2nd value provided by the user which called as a payload 
+ */
+
+function numberReducer(initialObject, action) {
+  if (action.type == "Increment") {
+    const numberToAdd = action.payload;
+    return {
+      ...initialObject,
+      number: initialObject.number + numberToAdd,
+    };
+  } else if (action.type == "Multiply") {
+    const numberToMultiply = action.payload;
+    return {
+      ...initialObject,
+      number: initialObject.number * numberToMultiply,
+    };
+  }
+}
+
+let obj = { number: 2 };
+
+console.log(numberReducer(obj, { type: "Increment", payload: 20 }));
+console.log(numberReducer(obj, { type: "Multiply", payload: 9 }));
